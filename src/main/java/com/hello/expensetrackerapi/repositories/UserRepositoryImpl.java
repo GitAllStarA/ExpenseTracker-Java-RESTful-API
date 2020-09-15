7037ed3b96b7c8ae2500b8406f98d2afd4c8ff5c
DS <37049888+GitAllStarA@users.noreply.github.com>
2020-09-14 21:01:45 -0500

    Added BCrypt

--- src/main/java/com/hello/expensetrackerapi/repositories/UserRepositoryImpl.java
+++ src/main/java/com/hello/expensetrackerapi/repositories/UserRepositoryImpl.java
@@ -50,7 +50,7 @@ public class UserRepositoryImpl implements UserRepository{
     public User findByEmailAndPassword(String email, String password) throws EtAuthException {
         try {
             User user = jdbcTemplate.queryForObject(SQL_FIND_BY_EMAIL,new Object[]{email}, userRowMapper);
-            if(!password.equals(user.getPassword()))
+            if(!BCrypt.checkpw(password,user.getPassword()))
                 throw new EtAuthException("Invalid password");
             return user;
         }catch (EmptyResultDataAccessException e) {
